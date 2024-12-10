4a5029399320103959f346f3c11f0c24a27327c5
Hong Xu <hong@8hobbies.com>
2024-12-09 16:21:04 -0800

    Add basic functionality to alter URL (#3)

--- src/main.test.ts
+++ src/main.test.ts
@@ -15,6 +15,51 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-test("Placeholder", () => {
-  // Placeholder
+
+import { SiteData, amazonComParam, builtinSiteData } from "./site_data";
+import { generateActivatingUrl, generateDisablingUrl } from "./main";
+
+describe("generate Url", () => {
+  const activatingResult = "https://example.com/?result=activated" as const;
+  const disablingResult = "https://example.com/?notactivated" as const;
+  const matchingUrl = "https://example.com";
+  const unmatchingUrl = "https://example.org";
+  const siteData = [
+    {
+      name: "Test Example",
+      urlRegex: /https:\/\/example\.com/,
+      activatingFunc: (): string => activatingResult,
+      disablingFunc: (): string => disablingResult,
+    },
+  ] as const satisfies SiteData;
+
+  test("Returns activating function result if URL matches", () => {
+    expect(generateActivatingUrl(matchingUrl, siteData)).toBe(activatingResult);
+  });
+
+  test("Returns disabling function result if URL matches", () => {
+    expect(generateDisablingUrl(matchingUrl, siteData)).toBe(disablingResult);
+  });
+
+  for (const func of [generateActivatingUrl, generateDisablingUrl] as const) {
+    test("Returns null if no URL matches", () => {
+      expect(func(unmatchingUrl, siteData)).toBeNull();
+    });
+  }
+
+  describe("builtin sitedata", () => {
+    test("Amazon.com activating matched", () => {
+      expect(
+        generateActivatingUrl("https://www.amazon.com/s?", builtinSiteData),
+      ).toBe(
+        `https://www.amazon.com/s?${amazonComParam.key}=${encodeURIComponent(amazonComParam.value)}`,
+      );
+    });
+
+    test("Amazon.com disabling matched", () => {
+      expect(
+        generateDisablingUrl("https://www.amazon.com/s?rh=", builtinSiteData),
+      ).toBe("https://www.amazon.com/s");
+    });
+  });
 });
